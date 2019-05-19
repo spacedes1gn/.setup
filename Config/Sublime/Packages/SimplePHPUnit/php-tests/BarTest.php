7c75c5f31235c1b3189dfe1921531cb3a9c6a49f
Max <spacedesigns@protonmail.com>
2019-05-19 16:40:30 +0100

    Removed Sublime Packages

--- Config/Sublime/Packages/SimplePHPUnit/php-tests/BarTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class BarTest extends PHPUnit_Framework_Testcase
-{
-	public function testSomethingBar()
-	{
-		$this->markTestIncomplete('Incomplete');
-	}
-}
--- Config/Sublime/Packages/SimplePHPUnit/php-tests/FooTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class FooTest extends PHPUnit_Framework_Testcase
-{
-	public function testSomething()
-	{
-		$this->fail('FAILURE!');
-	}
-}
